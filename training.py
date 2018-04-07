9186e4f51a1d4d4c47f293eb499f937c4ffeb69e
Albert <me@albert.sh>
2018-04-07 22:24:48 +0200

    training.py

---
+++ training.py
@@ -0,0 +1,27 @@
+from chatterbot import ChatBot
+'''
+chatbot = ChatBot(
+    'Ron Obvious',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+)
+
+'''
+
+
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatterbot = ChatBot("Training Example",
+        input_adapter='chatterbot.input.TerminalAdapter',
+    output_adapter='chatterbot.output.TerminalAdapter')
+chatterbot.set_trainer(ChatterBotCorpusTrainer)
+
+chatterbot.train(
+    "bot/chatterbot.json"
+)
+
+while True:
+    try:
+     bot_input = chatterbot.get_response(None)
+
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
